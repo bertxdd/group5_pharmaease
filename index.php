db633b463ad20463bebdebc8b0dd26c139ab3278
bertxdd <fpadios68@gmail.com>
2025-02-16 21:04:11 +0800

    v1

--- index.php
+++ index.php
@@ -1,33 +1,47 @@
-<?php
-    
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PharmaEase</title>
-    <link rel = "stylesheet" href = "style.css">
+    <link rel="stylesheet" href="style.css">
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Days+One&family=Lexend:wght@100..900&family=Teko:wght@300..700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Days+One&family=Lexend:wght@100..900&family=Teko:wght@300..700&display=swap');
     </style>
 </head>
+
 <body>
-    <section id = "homebg">
-        <section id = "header">
-            <img src = "Frame 6.png">
+    <section id="homebg">
+        <section id="header">
+            <img src="Frame 6.png">
 
         </section>
-        <section id = "welcome">
+        <section id="welcome">
             <h1>Welcome to PharmaEase <br>
-            A service you can trust</h1>
-            
+                A service you can trust</h1>
+
             <div class="frame-11">
                 <button class="check-discount-offer">Check Discount Offer</button>
             </div>
         </section>
+
+        <section id="navigation">
+            <div class="over-the-counter">
+                <div class="rectangle-17"></div>
+                <div class="over-the-counter2">Over The Counter</div>
+                <div class="group-2">
+                    <div class="frame-12">
+                        <div class="view">View</div>
+                    </div>
+                </div>
+                <div class="lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-sed-do-eiusmod-tempor-incididunt-ut-labore-et-dolore-magna-aliqua">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua
+                </div>
+            </div>
+        </section>
     </section>
 </body>
+
 </html>
