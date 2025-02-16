191b44347903bcfb805257daf7ee40f1c911397c
bertxdd <fpadios68@gmail.com>
2025-02-16 20:25:46 +0800

    first page

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PharmaEase</title>
+    <link rel = "stylesheet" href = "style.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Days+One&family=Lexend:wght@100..900&family=Teko:wght@300..700&display=swap');
+    </style>
+</head>
+<body>
+    <section id = "homebg">
+        <section id = "header">
+            <img src = "Frame 6.png">
+
+        </section>
+        <section id = "welcome">
+            <h1>Welcome to PharmaEase <br>
+            A service you can trust</h1>
+            
+            <div class="frame-11">
+                <button class="check-discount-offer">Check Discount Offer</button>
+            </div>
+        </section>
+    </section>
+</body>
+</html>
