b63873db66381ddcf87e9ac582e0e89c9caef62c
bertxdd <fpadios68@gmail.com>
2025-02-17 21:56:13 +0800

    finished nav boxes

--- index.php
+++ index.php
@@ -24,18 +24,32 @@
                 <button class="check-discount-offer">Check Discount Offer</button>
             </div>
         </section>
-        <section id="navigation">
+        <section id = "whitespace">
+            <br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br>
+        </section>
+        
+    </section>
+    <section id="navigation">
             <div class="navbox" id="navbox1">
-                
+                <h2>Over the Counter</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua</p>
+                    <button id = "navbutton"> View </button>
             </div>
             <div class="navbox" id="navbox2">
-                
+                <h2>Prescription Needed</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua</p>
+                    <button id = "navbutton"> View </button>
             </div>
             <div class="navbox" id="navbox3">
-                
+                <h2>Herbal Medicines</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua</p>
+                    <button id = "navbutton"> View </button>
             </div>
         </section>
-    </section>
 </body>
 
 </html>
