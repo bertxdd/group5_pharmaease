bf822ce6aa4d65d707769265568422daf0c8f0c9
bertxdd <fpadios68@gmail.com>
2025-02-17 22:31:54 +0800

    navbar left

--- index.php
+++ index.php
@@ -24,32 +24,139 @@
                 <button class="check-discount-offer">Check Discount Offer</button>
             </div>
         </section>
-        <section id = "whitespace">
+        <section id="whitespace">
             <br><br><br><br><br><br><br><br>
             <br><br><br><br><br><br><br><br>
         </section>
-        
+
     </section>
     <section id="navigation">
-            <div class="navbox" id="navbox1">
-                <h2>Over the Counter</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua</p>
-                    <button id = "navbutton"> View </button>
+        <div class="navbox" id="navbox1">
+            <h2>Over the Counter</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua</p>
+            <button id="navbutton"> View </button>
+        </div>
+        <div class="navbox" id="navbox2">
+            <h2>Prescription Needed</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua</p>
+            <button id="navbutton"> View </button>
+        </div>
+        <div class="navbox" id="navbox3">
+            <h2>Herbal Medicines</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua</p>
+            <button id="navbutton"> View </button>
+        </div>
+    </section>
+    <section id="pos">
+        <h1 id="popular">Popular Medicine</h1>
+    </section>
+    <section id="list">
+        <div class="medicine-list">
+            <img class="frame-13" src="Frame 13.png" />
+            <div class="medtitle">Paracetamol Biogesic</div>
+            <div class="medprice">₱6.45</div>
+            <div class="meddesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             </div>
-            <div class="navbox" id="navbox2">
-                <h2>Prescription Needed</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua</p>
-                    <button id = "navbutton"> View </button>
+            <button title="Add New" class="add-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24"
+                    class="button-icon">
+                    <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                        stroke-width="1.5"></path>
+                    <path d="M8 12H16" stroke-width="1.5"></path>
+                    <path d="M12 16V8" stroke-width="1.5"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="medicine-list">
+            <img class="frame-13" src="Frame 14.png" />
+            <div class="medtitle">BioFlu</div>
+            <div class="medprice">₱6.45</div>
+            <div class="meddesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             </div>
-            <div class="navbox" id="navbox3">
-                <h2>Herbal Medicines</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua</p>
-                    <button id = "navbutton"> View </button>
+            <button title="Add New" class="add-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24"
+                    class="button-icon">
+                    <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                        stroke-width="1.5"></path>
+                    <path d="M8 12H16" stroke-width="1.5"></path>
+                    <path d="M12 16V8" stroke-width="1.5"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="medicine-list">
+            <img class="frame-13" src="Frame 15.png" />
+            <div class="medtitle">Cetirizine</div>
+            <div class="medprice">₱8.45</div>
+            <div class="meddesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             </div>
-        </section>
+            <button title="Add New" class="add-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24"
+                    class="button-icon">
+                    <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                        stroke-width="1.5"></path>
+                    <path d="M8 12H16" stroke-width="1.5"></path>
+                    <path d="M12 16V8" stroke-width="1.5"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="medicine-list">
+            <img class="frame-13" src="Frame 16.png" />
+            <div class="medtitle">Celebrex</div>
+            <div class="medprice">₱17.25</div>
+            <div class="meddesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            </div>
+            <button title="Add New" class="add-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24"
+                    class="button-icon">
+                    <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                        stroke-width="1.5"></path>
+                    <path d="M8 12H16" stroke-width="1.5"></path>
+                    <path d="M12 16V8" stroke-width="1.5"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="medicine-list">
+            <img class="frame-13" src="Frame 17.png" />
+            <div class="medtitle">Metformin</div>
+            <div class="medprice">₱13.00</div>
+            <div class="meddesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            </div>
+            <button title="Add New" class="add-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24"
+                    class="button-icon">
+                    <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                        stroke-width="1.5"></path>
+                    <path d="M8 12H16" stroke-width="1.5"></path>
+                    <path d="M12 16V8" stroke-width="1.5"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="medicine-list">
+            <img class="frame-13" src="Frame 18.png" />
+            <div class="medtitle">Medicol Ibuprofen</div>
+            <div class="medprice">₱24.00</div>
+            <div class="meddesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            </div>
+            <button title="Add New" class="add-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24"
+                    class="button-icon">
+                    <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                        stroke-width="1.5"></path>
+                    <path d="M8 12H16" stroke-width="1.5"></path>
+                    <path d="M12 16V8" stroke-width="1.5"></path>
+                </svg>
+            </button>
+        </div>
+    </section>
 </body>
 
 </html>
